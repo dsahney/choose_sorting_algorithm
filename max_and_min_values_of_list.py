624d2ee68994d0ae357356c0ef2ba4cbb52b97da
Dhruv Sahney <37184867+dsahney@users.noreply.github.com>
2018-04-25 10:24:35 -0400

    Update max_and_min_values_of_list.py

--- max_and_min_values_of_list.py
+++ max_and_min_values_of_list.py
@@ -1,10 +1,32 @@
 # This function will take a list, L, and determine its maximum and minimum values.
 
-def max_min(L):
+import bubble_sort
+import insertion_sort
+import selection_sort
+
+def max_min_bubble(L):
     ''' (list) -> tuple
-    Return a tuple that shows the min and max value of a list, respectively.
+    Return a tuple that uses the bubble sort algorithms, to determine the min and max value of a list, respectively.
     '''
     sorted_list =  bubble_sort(L)
+    max_value = L[-1]
+    min_value = L[0]
+    return (min_value, max_value)
+
+def max_min_insertion(L):
+    ''' (list) -> tuple
+    Return a tuple that uses the insertion sort algorithms, to determine the min and max value of a list, respectively.
+    '''
+    sorted_list =  insertion_sort(L)
+    max_value = L[-1]
+    min_value = L[0]
+    return (min_value, max_value)
+
+def max_min_selection(L):
+    ''' (list) -> tuple
+    Return a tuple that uses the selection sort algorithms, to determine the min and max value of a list, respectively.
+    '''
+    sorted_list =  selection_sort(L)
     max_value = L[-1]
     min_value = L[0]
     return (min_value, max_value)
